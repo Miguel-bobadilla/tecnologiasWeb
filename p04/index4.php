5541e1bf04d83fc7f4160d3a757a2ef83fb4181e
Miguel-bobadilla <97259732+Miguel-bobadilla@users.noreply.github.com>
2023-09-20 23:10:46 -0600

    practica 4

---
+++ p04/index4.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Práctica 4</title>
+</head>
+<body>
+    <h1>Ejercicios</h1>
+
+    <h2>Ejercicio 1</h2>
+    
+    <?php
+    include 'funciones.php';
+    $resultadoEjercicio1 = ejercicio1();
+    echo $resultadoEjercicio1;
+    ?>
+
+    <h2>Ejercicio 2</h2>
+ 
+    <?php
+    $resultadoEjercicio2 = ejercicio2();
+    echo $resultadoEjercicio2;
+    ?>
+
+<h2>Ejercicio 3</h2>
+ 
+    <?php
+    $resultadoEjercicio2 = ejercicio3();
+    echo $resultadoEjercicio2;
+    ?>
+
+<h2>Ejercicio 4</h2>
+
+    <?php
+    $resultadoEjercicio2 = ejercicio4();
+    echo $resultadoEjercicio2;
+    ?>
+
+    <!-- Continúa con los demás ejercicios -->
+
+</body>
+</html>
